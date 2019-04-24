ecb8755f4f11bb91b957127432c850be8d0a9ff9
Boy Maas <boy.maas@gmail.com>
2019-04-24 21:53:24 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.monetashi.yesod "0.1.0"
+(defproject io.monetashi.yesod "0.1.1"
   :description "Foundational recurring themes"
   :url "https://monetashi.io"
   :license {:name "Eclipse Public License"
